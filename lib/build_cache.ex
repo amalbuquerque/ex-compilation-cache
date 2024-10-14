daeabd27cc7d7343008f40d8c0421ca51f940b02
André Albuquerque <andre.malbuq@gmail.com>
2024-10-14 17:53:50 +0100

    Feat: BuildCache provides search_prefix/1, parse/1, etc.

--- lib/build_cache.ex
+++ lib/build_cache.ex
@@ -24,7 +24,7 @@ defmodule ExCompilationCache.BuildCache do
     new(architecture, operating_system, mix_env, commit_hash)
   end
 
-  def new(architecture, operating_system, mix_env, commit_hash)
+  def new(architecture, operating_system, mix_env, commit_hash, timestamp \\ DateTime.utc_now())
       when architecture in @cacheable_architectures and
              operating_system in @cacheable_operating_systems and
              mix_env in @cacheable_mix_envs do
@@ -33,8 +33,70 @@ defmodule ExCompilationCache.BuildCache do
       operating_system: operating_system,
       mix_env: mix_env,
       commit_hash: commit_hash,
-      timestamp: DateTime.utc_now()
+      timestamp: timestamp
     }
+  end
+
+  def parse(artifact_name) do
+    # drop the extension
+    artifact_name = String.trim_trailing(artifact_name, Path.extname(artifact_name))
+
+    architecture = Enum.find(@cacheable_architectures, &String.contains?(artifact_name, to_string(&1)))
+    operating_system = Enum.find(@cacheable_operating_systems, &String.contains?(artifact_name, to_string(&1)))
+    mix_env = Enum.find(@cacheable_mix_envs, &String.contains?(artifact_name, to_string(&1)))
+
+    [timestamp_string, commit_hash | _] = artifact_name
+      |> String.split("_")
+      |> Enum.reverse()
+
+    timestamp = parse_timestamp(timestamp_string)
+
+    new(architecture, operating_system, mix_env, commit_hash, timestamp)
+  end
+
+  defp parse_timestamp(<<yyyy::binary-4, month::binary-2, dd::binary-2, hh::binary-2, minutes::binary-2, seconds::binary-2>>) do
+    {:ok, timestamp, 0} = DateTime.from_iso8601("#{yyyy}-#{month}-#{dd} #{hh}:#{minutes}:#{seconds}Z")
+
+    timestamp
+  end
+
+  def artifact_name(%__MODULE__{} = artifact) do
+    parts = [
+      artifact.architecture,
+      artifact.operating_system,
+      artifact.mix_env,
+      artifact.commit_hash,
+      Calendar.strftime(artifact.timestamp, "%Y%m%d%H%M%S")
+    ]
+
+    Enum.join(parts, "_")
+  end
+
+  def remote_artifact_path(%__MODULE__{} = artifact, extension) do
+    remote_path = Enum.join([
+      artifact.architecture,
+      artifact.mix_env,
+      artifact_name(artifact)
+    ], "/")
+
+    "#{remote_path}.#{extension}"
+  end
+
+  def search_prefix(%__MODULE__{} = artifact) do
+    parts = [
+      artifact.architecture,
+      artifact.operating_system,
+      artifact.mix_env,
+      artifact.commit_hash
+    ]
+
+    search_prefix = Enum.join(parts, "_")
+
+    Enum.join([
+      artifact.architecture,
+      artifact.mix_env,
+      search_prefix
+    ], "/")
   end
 
   # to simplify, we assume that only Linux laptops use x86_64, since
